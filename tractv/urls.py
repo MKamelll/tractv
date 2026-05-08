416a2f500e13fd725058d6b0bee957a0abe3b284
Mina Kamel <minallkamel@gmail.com>
2026-05-08 14:47:43 +0300

    added basic show info ui

--- tractv/urls.py
+++ tractv/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("shows/", include("shows.urls")),
     path("dashboard/", include("dashboard.urls")),
     path("admin/", admin.site.urls),
 ]
