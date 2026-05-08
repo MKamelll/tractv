46a4ed31a3155e1fcea5ce99f961574bb17abcbe
Mina Kamel <minallkamel@gmail.com>
2026-05-08 03:50:52 +0300

    added django-cotton

--- tractv/settings.py
+++ tractv/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "api",
     "common",
     "dashboard",
+    "django_cotton",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "node_modules",
+]
