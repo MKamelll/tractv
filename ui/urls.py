6924b9948e68063c464711815e87ce9b41a77a23
Mina Kamel <minallkamel@gmail.com>
2026-08-03 03:49:01 +0300

    maybe a searchbar

--- ui/urls.py
+++ ui/urls.py
@@ -4,6 +4,6 @@ from .views import shows, season, dashboard, search
 urlpatterns = [
     path("shows/<int:show_id>", view=shows, name="shows"),
     path("shows/<int:show_id>/seasons/<int:season_number>", view=season, name="season"),
-    path("dashboard/", view=dashboard, name="dashboard"),
     path("search/", view=search, name="search"),
+    path("", view=dashboard, name="dashboard"),
 ]
